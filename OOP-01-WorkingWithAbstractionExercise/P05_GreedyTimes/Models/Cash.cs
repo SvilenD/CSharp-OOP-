bc2d455c06ed3b04f05958ac67b6e86dad10d5a8
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-19 10:39:35 +0300

    exercises

---
+++ OOP-01-WorkingWithAbstractionExercise/P05_GreedyTimes/Models/Cash.cs
@@ -0,0 +1,10 @@
+﻿namespace GreedyTimes.Models
+{
+    public class Cash :Item
+    {
+        public Cash(string name, long quantity)
+            : base(name, quantity)
+        {
+        }
+    }
+}
---
+++ OOP-01-WorkingWithAbstractionExercise/P05_GreedyTimes/Models/Gem.cs
@@ -0,0 +1,10 @@
+﻿namespace GreedyTimes.Models
+{
+    public class Gem :Item
+    {
+        public Gem(string name, long quantity)
+        : base(name, quantity)
+        {
+        }
+    }
+}
