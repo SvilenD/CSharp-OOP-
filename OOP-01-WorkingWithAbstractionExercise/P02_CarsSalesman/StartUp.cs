ac80417f40ad6a21cd11e98a09aff7560610e375
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-19 10:43:21 +0300

    exercises

---
+++ OOP-01-WorkingWithAbstractionExercise/P02_CarsSalesman/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace CarsSalesman
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var catalogue = new Catalogue();
+
+            var enginesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < enginesCount; i++)
+            {
+                var parameters = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                catalogue.AddEngine(parameters);
+            }
+
+            int carsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                var parameters = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                catalogue.AddCar(parameters);
+            }
+
+            foreach (var car in catalogue.Cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
