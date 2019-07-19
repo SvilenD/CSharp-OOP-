e7d5eee20c3b6b962009795c69a9e6868d2d023d
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-19 10:42:39 +0300

    exercises

---
+++ OOP-01-WorkingWithAbstractionExercise/P02_CarsSalesman/CarFactory.cs
@@ -0,0 +1,46 @@
+﻿namespace CarsSalesman
+{
+    using System;
+
+    public class CarFactory
+    {
+        private string[] data;
+
+        public CarFactory(string[] data)
+        {
+            this.data = data;
+        }
+
+        public Car Create(Engine engine)
+        {
+            var model = data[0];
+
+            if (data.Length == 2)
+            {
+                return new Car(model, engine);
+            }
+
+            else if (data.Length == 3)
+            {
+                try
+                {
+                    var weight = int.Parse(data[2]);
+                    return new Car(model, engine, weight);
+                }
+                catch (Exception)
+                {
+                    var color = data[2];
+                    return new Car(model, engine, color);
+                }
+            }
+            else if (data.Length == 4)
+            {
+                var weight = int.Parse(data[2]);
+                string color = data[3];
+                return new Car(model, engine, weight, color);
+            }
+
+            return null;
+        }
+    }
+}
