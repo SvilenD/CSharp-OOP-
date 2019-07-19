8ca072cac7c9649b81009df2c4a2b64b55621ace
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-19 10:40:21 +0300

    exercises

---
+++ OOP-01-WorkingWithAbstractionExercise/P05_GreedyTimes/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace GreedyTimes.Models
+{
+    public abstract class Item
+    {
+        protected Item(string name, long quantity)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+        }
+
+        public string Name { get; }
+
+        public long Quantity { get; set; }
+    }
+}
