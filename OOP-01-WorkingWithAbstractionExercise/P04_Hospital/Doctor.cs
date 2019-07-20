80c8e73fe77100bc5aba856f8d20685eb2cba1dd
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-20 11:31:04 +0300

    Create Doctor.cs

---
+++ OOP-01-WorkingWithAbstractionExercise/P04_Hospital/Doctor.cs
@@ -0,0 +1,22 @@
+﻿namespace Hospital
+{
+    using System.Collections.Generic;
+
+    public class Doctor
+    {
+        public Doctor(string name)
+        {
+            this.Name = name;
+            this.Patients = new List<string>();
+        }
+
+        public string Name { get; }
+
+        public List<string> Patients { get; }
+
+        public void AddPatient(string patient)
+        {
+            this.Patients.Add(patient);
+        }
+    }
+}
