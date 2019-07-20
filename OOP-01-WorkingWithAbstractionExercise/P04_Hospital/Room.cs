2dc5bc150e44eb1ff15deb9676c0f7d9a6be84e8
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-20 11:29:51 +0300

    Create Room.cs

---
+++ OOP-01-WorkingWithAbstractionExercise/P04_Hospital/Room.cs
@@ -0,0 +1,35 @@
+﻿namespace Hospital
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Room
+    {
+        private const int capacity = 3;
+
+        public Room()
+        {
+            this.Patients = new List<string>(capacity);
+        }
+
+        public List<string> Patients { get; }
+
+        public void Add(string name)
+        {
+            if (this.Patients.Count < capacity)
+            {
+                Patients.Add(name);
+            }
+        }
+
+        public bool HasFreeBed()
+        {
+            return this.Patients.Count < capacity;
+        }
+
+        public override string ToString()
+        {
+            return $"{string.Join(Environment.NewLine, Patients)}";
+        }
+    }
+}
