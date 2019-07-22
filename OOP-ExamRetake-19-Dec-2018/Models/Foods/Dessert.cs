47e554c78683811812d689cd7539361f5396bcf1
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-22 17:37:27 +0300

    Create Dessert.cs

---
+++ OOP-ExamRetake-19-Dec-2018/Models/Foods/Dessert.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftUniRestaurant.Models.Foods
+{
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price)
+            : base(name, (int)ServingSizes.Dessert, price)
+        {
+        }
+    }
+}
