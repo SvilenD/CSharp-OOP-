a80e71c5acbed4fc2d82c4ffa82c67706efd9d4c
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-22 17:39:35 +0300

    Create FuzzyDrink.cs

---
+++ OOP-ExamRetake-19-Dec-2018/Models/Drinks/FuzzyDrink.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftUniRestaurant.Models.Drinks
+{
+    public class FuzzyDrink : Drink
+    {
+
+        public FuzzyDrink(string name, int servingSize, string brand)
+            : base(name, servingSize, Prices.FuzzyDrink, brand)
+        {
+        }
+    }
+}
