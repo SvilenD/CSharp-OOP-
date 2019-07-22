dcd967b94bddb0e728ca2199af90d6b19cdb4ef5
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-22 17:39:21 +0300

    Create Juice.cs

---
+++ OOP-ExamRetake-19-Dec-2018/Models/Drinks/Juice.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftUniRestaurant.Models.Drinks
+{
+    public class Juice : Drink
+    {
+        public Juice(string name, int servingSize, string brand)
+            : base(name, servingSize, Prices.Juice, brand)
+        {
+        }
+    }
+}
