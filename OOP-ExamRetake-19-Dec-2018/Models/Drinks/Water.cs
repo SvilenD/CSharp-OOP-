a083f2738f26bb43d622a6246af7e424267e2b28
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-22 17:39:06 +0300

    Create Water.cs

---
+++ OOP-ExamRetake-19-Dec-2018/Models/Drinks/Water.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftUniRestaurant.Models.Drinks
+{
+    public class Water : Drink
+    {
+        public Water(string name, int servingSize, string brand)
+            : base(name, servingSize, Prices.Water, brand)
+        {
+        }
+    }
+}
