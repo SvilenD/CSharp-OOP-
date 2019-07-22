e3300e5ce45b18c2c3af7b214bffe358ff06b853
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-22 12:20:01 +0300

    Create RandomList.cs

---
+++ OOP-02-Inheritance/RandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RandomList : List<string>
+    {
+        private readonly Random random = new Random();
+
+        public string RandomString()
+        {
+            var index = random.Next(0, this.Count);
+
+            var item = this[index];
+            this.Remove(item);
+
+            return item;
+        }
+    }
+}
