c44cb68c1c885ce1d010dd8b35fbb71535960b73
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-22 17:43:01 +0300

    Create ExceptionMessages.cs

---
+++ OOP-ExamRetake-19-Dec-2018/ExceptionMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace SoftUniRestaurant
+{
+    public class ExceptionMessages
+    {
+        public const string NULL_Name = "Name cannot be null or white space!";
+
+        public const string NEGATIVE_ServingSize = "Serving size cannot be less or equal to zero";
+
+        public const string NEGATIVE_Price = "Price cannot be less or equal to zero!";
+
+        public const string NULL_BrandName = "Brand cannot be null or white space!";
+
+        public const string NEGATIVE_TableCapacity = "Capacity has to be greater than 0";
+
+        public const string NEGATIVE_NumberOfPeople = "Cannot place zero or less people!";
+    }
+}
