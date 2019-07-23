e065b24ba752a520b3582e2c468f2d222038fd44
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-23 13:34:20 +0300

    Update Beginner.cs

--- OOP-RetakeExam-18 Apr-2019/PlayersAndMonsters/Models/Players/Beginner.cs
+++ OOP-RetakeExam-18 Apr-2019/PlayersAndMonsters/Models/Players/Beginner.cs
@@ -1,7 +1,7 @@
-﻿using PlayersAndMonsters.Repositories.Contracts;
-
-namespace PlayersAndMonsters.Models.Players
+﻿namespace PlayersAndMonsters.Models.Players
 {
+    using PlayersAndMonsters.Repositories.Contracts;
+
     public class Beginner : Player
     {
         private const int InitialHealth = 50;
