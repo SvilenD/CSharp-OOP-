74cd8427943adca4fddb268a956d6e0ea60a2ba1
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-24 15:17:05 +0300

    exercises

---
+++ OOP-02-InheritanceExercise/Person/Child.cs
@@ -0,0 +1,28 @@
+﻿namespace Person
+{
+    using System;
+
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+            this.Age = base.Age ;
+        }
+
+        public new int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            private set
+            {
+                if (base.Age > 15)
+                {
+                    throw new ArgumentException($"Age should be less than 15.");
+                }
+            }
+        }
+    }
+}
---
+++ OOP-02-InheritanceExercise/Person/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Person
+{
+    using System;
+
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"Age should be positive");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
