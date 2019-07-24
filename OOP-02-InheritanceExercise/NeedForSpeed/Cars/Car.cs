b0591cad5d001f150d0d1a5301419fd10edeef68
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-24 15:16:45 +0300

    exercises

---
+++ OOP-02-InheritanceExercise/NeedForSpeed/Cars/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace NeedForSpeed.Cars
+{
+    public class Car : Vehicle
+    {
+        private const double DEFAULT_FuelConsumption = 3;
+
+        public Car(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption()
+        {
+            return DEFAULT_FuelConsumption;
+        }
+    }
+}
---
+++ OOP-02-InheritanceExercise/NeedForSpeed/Cars/SportCar.cs
@@ -0,0 +1,17 @@
+﻿namespace NeedForSpeed.Cars
+{
+    public class SportCar : Car
+    {
+        private const double DEFAULT_FuelConsumption = 10;
+
+        public SportCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption()
+        {
+            return DEFAULT_FuelConsumption;
+        }
+    }
+}
