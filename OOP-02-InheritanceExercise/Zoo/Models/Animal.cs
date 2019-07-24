f8e5f4a786dad2582e444688a218ecc22f302c55
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-24 15:15:07 +0300

    exercises

---
+++ OOP-02-InheritanceExercise/Zoo/Models/Animal.cs
@@ -0,0 +1,28 @@
+﻿namespace Zoo.Models
+{
+    public class Animal
+    {
+        private string name;
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value !=null)
+                {
+                    this.name = value;
+                }
+            }
+        }
+    }
+}
