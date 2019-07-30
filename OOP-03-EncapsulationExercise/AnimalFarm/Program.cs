f845a3ebee84c35c31348b12e225febcd0b04dd1
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-30 08:57:08 +0300

    exercise

---
+++ OOP-03-EncapsulationExercise/AnimalFarm/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+
+    class Program
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var chicken = new Chicken(name, age);
+                Console.WriteLine(chicken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
