33e72a1f8f29972b545a5552737e5ea68c0adbe7
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-07-31 10:58:23 +0300

    Update Engine.cs

--- OOP-Exam-14-Apr-2019/Skeleton/MortalEngines/Core/Engine.cs
+++ OOP-Exam-14-Apr-2019/Skeleton/MortalEngines/Core/Engine.cs
@@ -1,8 +1,8 @@
 ﻿namespace MortalEngines.Core
 {
-    using MortalEngines.Core.Contracts;
-    using MortalEngines.IO;
     using System;
+    using MortalEngines.IO;
+    using MortalEngines.Core.Contracts;
 
     public class Engine : IEngine
     {
@@ -29,44 +29,53 @@
                     {
                         case "HirePilot":
                             var name = command.Params[0];
+
                             writer.Write(machinesManager.HirePilot(name));
                             break;
                         case "PilotReport":
                             name = command.Params[0];
+
                             writer.Write(machinesManager.PilotReport(name));
                             break;
                         case "ManufactureTank":
                             name = command.Params[0];
                             var attack = double.Parse(command.Params[1]);
                             var defense = double.Parse(command.Params[2]);
+
                             writer.Write(machinesManager.ManufactureTank(name, attack, defense));
                             break;
                         case "ManufactureFighter":
                             name = command.Params[0];
                             attack = double.Parse(command.Params[1]);
                             defense = double.Parse(command.Params[2]);
+
                             writer.Write(machinesManager.ManufactureFighter(name, attack, defense));
                             break;
                         case "MachineReport":
                             name = command.Params[0];
+
                             writer.Write(machinesManager.MachineReport(name));
                             break;
                         case "AggressiveMode":
                             name = command.Params[0];
+
                             writer.Write(machinesManager.ToggleFighterAggressiveMode(name));
                             break;
                         case "DefenseMode":
                             name = command.Params[0];
+
                             writer.Write(machinesManager.ToggleTankDefenseMode(name));
                             break;
                         case "Engage":
                             var pilotName = command.Params[0];
                             var machineName = command.Params[1];
+
                             writer.Write(machinesManager.EngageMachine(pilotName, machineName));
                             break;
                         case "Attack":
                             var attacker = command.Params[0];
                             var defender = command.Params[1];
+
                             writer.Write(machinesManager.AttackMachines(attacker, defender));
                             break;
                     }
