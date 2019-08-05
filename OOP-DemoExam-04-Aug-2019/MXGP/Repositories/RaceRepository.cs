3d2000da44b1fad81535806c395b56a9f8c43ae0
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-05 10:38:42 +0300

    Create RaceRepository.cs

---
+++ OOP-DemoExam-04-Aug-2019/MXGP/Repositories/RaceRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace MXGP.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+    using MXGP.Models.Races.Contracts;
+
+    public class RaceRepository : Repository<IRace>
+    {
+        public RaceRepository()
+        {
+            this.models = new List<IRace>();
+        }
+
+        public override IRace GetByName(string name)
+        {
+            return this.models.FirstOrDefault(m => m.Name == name);
+        }
+    }
+}
