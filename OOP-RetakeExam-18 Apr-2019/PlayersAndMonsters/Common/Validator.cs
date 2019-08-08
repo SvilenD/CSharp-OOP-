ec3f65dd0fc0eccb9a7f10ff7562614e82089623
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-08 14:31:01 +0300

    Create Validator.cs

---
+++ OOP-RetakeExam-18 Apr-2019/PlayersAndMonsters/Common/Validator.cs
@@ -0,0 +1,23 @@
+﻿namespace PlayersAndMonsters.Common
+{
+    using System;
+
+    public static class Validator
+    {
+        public static void ThrowsExceptionIfNegativeValue(int value, string msg)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(msg);
+            }
+        }
+
+        public static void ThrowsExceptionIfStringNullOrEmpty(string value, string msg)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException(msg);
+            }
+        }
+    }
+}
