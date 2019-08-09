ab06d76cd2ad11b0c7691f44ff84f1807926553c
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-09 16:11:30 +0300

    Create CommandInterpreter.cs

---
+++ OOP-DemoExam-04-Aug-2019/MXGP/Core/CommandInterpreter.cs
@@ -0,0 +1,43 @@
+﻿namespace MXGP.Core
+{
+    using System;
+    using MXGP.Core.Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private readonly ChampionshipController controller;
+
+        public CommandInterpreter()
+        {
+            this.controller = new ChampionshipController();
+        }
+
+        public string Execute(string[] input)
+        {
+            try
+            {
+                switch (input[0])
+                {
+                    case "CreateRider":
+                        return controller.CreateRider(input[1]);
+                    case "CreateMotorcycle":
+                        return controller.CreateMotorcycle(input[1], input[2], int.Parse(input[3]));
+                    case "AddMotorcycleToRider":
+                        return controller.AddMotorcycleToRider(input[1], input[2]);
+                    case "AddRiderToRace":
+                        return controller.AddRiderToRace(input[1], input[2]);
+                    case "CreateRace":
+                        return controller.CreateRace(input[1], int.Parse(input[2]));
+                    case "StartRace":
+                        return controller.StartRace(input[1]);
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
