f14bbac179d2014b41ec3272f44c3f51e741e1d5
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-09 16:11:14 +0300

    Create Validator.cs

---
+++ OOP-DemoExam-04-Aug-2019/MXGP/Utilities/Validator.cs
@@ -0,0 +1,15 @@
+﻿namespace MXGP.Utilities
+{
+    using System;
+
+    public static class Validator
+    {
+        public static void ValidateString(string value, int minLength, string msg)
+        {
+            if (String.IsNullOrWhiteSpace(value) || value.Length < minLength)
+            {
+                throw new ArgumentException(String.Format(msg, value, minLength));
+            }
+        }
+    }
+}
