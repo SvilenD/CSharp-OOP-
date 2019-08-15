8791e23d9d6c15e04f8c1af62148df9cf17c3467
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-15 16:39:09 +0300

    Update ExceptionMessages.cs

--- OOP-Exam-11-Aug-2019/ViceCity/Core/ExceptionMessages.cs
+++ OOP-Exam-11-Aug-2019/ViceCity/Core/ExceptionMessages.cs
@@ -2,16 +2,14 @@
 {
     public class ExceptionMessages
     {
-        public const string GUN_NameInvalid = "Name cannot be null or a white space!";
+        public const string GunNameInvalid = "Name cannot be null or a white space!";
 
-        public const string GUN_BulletsNegative = "Bullets cannot be below zero!";
+        public const string GunBulletsNegative = "Bullets cannot be below zero!";
 
-        public const string GUN_TotalBulletsNegative = "Total bullets cannot be below zero!";
+        public const string GunTotalBulletsNegative = "Total bullets cannot be below zero!";
 
-        public const string PLAYER_NameInvalid = "Player's name cannot be null or a whitespace!";
+        public const string PlayerNameInvalid = "Player's name cannot be null or a whitespace!";
 
-        public const string PLAYER_LifePointsNegative = "Player life points cannot be below zero!";
-
-
+        public const string PlayerLifePointsNegative = "Player life points cannot be below zero!";
     }
 }
