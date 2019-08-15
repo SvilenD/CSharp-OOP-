5dbd1cb901fd3e7f59f71ae71b64fa55c9b13e50
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-15 16:39:05 +0300

    Update OutputMessages.cs

--- OOP-Exam-11-Aug-2019/ViceCity/Core/OutputMessages.cs
+++ OOP-Exam-11-Aug-2019/ViceCity/Core/OutputMessages.cs
@@ -2,8 +2,30 @@
 {
     public class OutputMessages
     {
-        public const string GUN_Added = "Successfully added {0} of type: {1}";
+        public const string GunAdded = "Successfully added {0} of type: {1}";
 
-        public const string GUN_InvalidType = "Invalid gun type!";
+        public const string GunInvalidType = "Invalid gun type!";
+
+        public const string NoGuns = "There are no guns in the queue!";
+
+        public const string MainPlayerAddedGun = "Successfully added {0} to the Main Player: Tommy Vercetti";
+
+        public const string MainPlayerName = "Vercetti";
+
+        public const string PlayerNotExists = "Civil player with that name doesn't exists!";
+
+        public const string PlayerAddedGun = "Successfully added {0} to the Civil Player: {1}";
+
+        public const string CivilPlayerAdded = "Successfully added civil player: {0}!";
+
+        public const string AllOk = "Everything is okay!";
+
+        public const string Fight = "A fight happened:";
+
+        public const string LifePoints = "Tommy live points: {0}!";
+
+        public const string Killed = "Tommy has killed: {0} players!";
+
+        public const string CivilsLeft = "Left Civil Players: {0}!";
     }
 }
