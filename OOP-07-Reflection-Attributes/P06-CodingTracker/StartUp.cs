ee8664a16b507d9fd19e677f6f10f395467550bc
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-17 13:16:37 +0300

    Task6

---
+++ OOP-07-Reflection-Attributes/P06-CodingTracker/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+[Author("Ventsi")]
+class StartUp
+{
+    [Author("Gosho")]
+    static void Main()
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
---
+++ OOP-07-Reflection-Attributes/P06-CodingTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+[Author("Svilen")]
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var methods = assembly
+            .GetTypes()
+            .SelectMany(x => x.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static));
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+            {
+                foreach (AuthorAttribute attribute in method.GetCustomAttributes())
+                {
+                    Console.WriteLine($"{method.Name} is writen by {attribute.Name}" );
+
+                }
+            }
+        }
+    }
+}
