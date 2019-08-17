3107ba9837fe4c063edfcf72009fc5b186b6a79f
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-17 13:16:14 +0300

    Task4

---
+++ OOP-07-Reflection-Attributes/P04-Collector/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    private readonly StringBuilder data;
+
+    public Spy()
+    {
+        this.data = new StringBuilder();
+    }
+
+    public string CollectGettersAndSetters(string investigatedClass)
+    {
+        Type classType = Type.GetType(investigatedClass);
+
+        MethodInfo[] methods = classType.GetMethods( BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        foreach (var getter in methods.Where(m=>m.Name.StartsWith("get")))
+        {
+            data.AppendLine($"{getter.Name} will return {getter.ReturnType}"); // ReturnParameter.ParameterType
+
+        }
+        foreach (var setter in methods.Where(m => m.Name.StartsWith("set")))
+        {
+            data.AppendLine($"{setter.Name} will set field of {setter.GetParameters().First().ParameterType}");
+        }
+        return this.data.ToString().Trim();
+    }
+}
