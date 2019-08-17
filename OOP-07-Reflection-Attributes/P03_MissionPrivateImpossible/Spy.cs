254391e531f7bb2ee0ca08f70d417a1e590dc935
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-17 13:16:03 +0300

    Task3

---
+++ OOP-07-Reflection-Attributes/P03_MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    private readonly StringBuilder data;
+
+    public Spy()
+    {
+        this.data = new StringBuilder();
+    }
+
+    public string RevealPrivateMethods(string investigatedClass)
+    {
+        Type classType = Type.GetType(investigatedClass);
+        MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+        data.AppendLine($"All Private Methods of Class: {investigatedClass}")
+            .AppendLine($"Base Class: {classType.BaseType.Name}");
+
+        foreach (MethodInfo method in classMethods)
+        {
+            data.AppendLine(method.Name);
+        }
+
+        return data.ToString().Trim();
+    }
+}
