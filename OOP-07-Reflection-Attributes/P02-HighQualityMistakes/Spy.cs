9796d7cdc5d00ee6e33f89d6c7329bdc3128e701
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-17 13:15:48 +0300

    Task2

---
+++ OOP-07-Reflection-Attributes/P02-HighQualityMistakes/Spy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    private readonly StringBuilder data;
+
+    public Spy()
+    {
+        this.data = new StringBuilder();
+    }
+
+    public string AnalyzeAcessModifiers(string className)
+    {
+        Type classType = Type.GetType(className);
+        FieldInfo[] fieldsInfo = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+        foreach (var field in fieldsInfo)
+        {
+            data.AppendLine($"{field.Name} must be private!");
+        }
+
+        MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+        foreach (var method in publicMethods.Where(m => m.Name.StartsWith("get")))
+        {
+            data.AppendLine($"{method.Name} have to be public!");
+        }
+
+        MethodInfo[] nonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+        foreach (var method in nonPublicMethods.Where(m=>m.Name.StartsWith("set")))
+        {
+            data.AppendLine($"{method.Name} have to be private!");
+        }
+
+        return data.ToString().Trim();
+    }
+}
