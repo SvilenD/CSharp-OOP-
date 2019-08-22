c4342d3458e18a216d6dadbdccfca0831698d5cd
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-22 14:26:53 +0300

    created

---
+++ OOP-07-Reflection-Attributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,27 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using CommandPattern.Core.Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string CommandNamePostFix = "Command";
+
+        public string Read(string args)
+        {
+            var commandArgs = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var className = commandArgs[0] + CommandNamePostFix;
+
+            var assembly = Assembly.GetCallingAssembly();
+
+            var type = assembly.GetTypes().FirstOrDefault(x=>x.Name == className);
+
+            var instance = (ICommand)Activator.CreateInstance(type);
+
+            return instance.Execute(commandArgs.Skip(1).ToArray());
+        }
+    }
+}
---
+++ OOP-07-Reflection-Attributes-Exercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using CommandPattern.Core.Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            var command = Console.ReadLine();
+
+            Console.WriteLine(commandInterpreter.Read(command));
+        }
+    }
+}
