8e4c27fec6f347296d61bdf9342de8064051d443
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-08-22 14:26:23 +0300

    AttributesTask

---
+++ OOP-07-Reflection-Attributes-Exercise/ValidationAttributes/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace ValidationAttributes
+{
+    using ValidationAttributes.Attributes;
+
+    public class Person
+    {
+        private const int MIN_Value = 12;
+        private const int MAX_Value = 90;
+
+        public Person(string fullName, int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+        [MyRequiredAttribute]
+        public string FullName { get; private set; }
+
+        [MyRangeAttribute(MIN_Value, MAX_Value)]
+        public int Age { get; private set; }
+    }
+}
---
+++ OOP-07-Reflection-Attributes-Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,32 @@
+﻿namespace ValidationAttributes
+{
+    using System.Linq;
+    using System.Reflection;
+    using ValidationAttributes.Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            var properties = obj.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+            foreach (var prop in properties)
+            {
+                var attributes = prop.GetCustomAttributes()
+                                .Where(a => a is MyValidationAttribute)
+                                .Select(a => (MyValidationAttribute)a);
+
+                foreach (MyValidationAttribute attribute in attributes)
+                {
+                    if (attribute.IsValid(prop.GetValue(obj)) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
